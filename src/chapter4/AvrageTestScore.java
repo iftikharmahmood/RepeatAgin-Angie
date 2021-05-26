570b189a7dbff351456bf7d8080b834d99770992
Iftikhar Mahmood <47450543+iftikharmahmood@users.noreply.github.com>
2021-05-26 09:21:15 +0200

    løkke i løkke

--- src/chapter4/AvrageTestScore.java
+++ src/chapter4/AvrageTestScore.java
@@ -1,5 +1,7 @@
 package chapter4;
 
+import java.util.Scanner;
+
 /**
  * Recap Nestet LOOPS
  */
@@ -10,10 +12,24 @@ public class AvrageTestScore {
         int antallStudenter = 24;
         int antallPrøver = 4;
 
+        Scanner scanner = new Scanner(System.in);
+
         for (int i = 0; i < antallStudenter; i++) {
+
+            System.out.println("Karakterer for student nr " + (i+1));
             double total = 0;
 
+            for (int j = 0; j < antallPrøver; j++) {
+                System.out.println("Tast inn karakteren for fag nr " +(j+1));
+                double karakter = scanner.nextDouble();
+                total = total + karakter;
+
+            }
+            double snitt = total / antallPrøver;
+            System.out.println(" Snitt karakteren for student nr " + (i+1) + " er " + snitt);
+
         }
+        scanner.close();
 
     }
 }
